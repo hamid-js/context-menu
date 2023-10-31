69481251e410a25d7788b11cc4831fbfd45683e8
Hamidreza Mohamadi <hamid.reactjs@gmail.com>
2023-10-31 21:27:09 +0330

    ready to upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# SpongeBob Form
+
+A beautiful creative Context Menu. Look, use and enjoy.
+[click & show](https://hamid-js.github.io/context/)
+
+## Technologies
+
+- HTML5
+- CSS3
+- JavaScript 
---
+++ js/script.js
@@ -0,0 +1,20 @@
+﻿let contextMenu = document.getElementById("contextMenu");
+
+
+
+document.body.addEventListener("contextmenu",newMenuFunc);
+document.body.addEventListener("click",delContextMenu);
+
+function newMenuFunc (event) {
+  
+contextMenu.style.display="block";
+contextMenu.style.left= event.pageX
+contextMenu.style.top=event.pageY
+
+console.log(event.pageX,event.pageY);
+console.log(event);
+}
+function delContextMenu (){
+    contextMenu.style.display="none";
+
+}
